50f93ff851d014eab9908f89cf86cc2a1ed3fd10
rmohan11 <rmohan11@asu.edu>
2018-09-22 20:04:00 -0700

    Removed the virtual environment as it was not necessary and added the files related to new app created booking

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rakesh/Documents/ASU/Classes/Sem3/SER515/Project/virat/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
