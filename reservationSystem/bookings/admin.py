001802f888d54cbef85727939d6b472f02989afc
rmohan11 <rmohan11@asu.edu>
2018-10-02 12:07:43 -0700

    Made change to access models from admin page, to font and styles in index page and setup to launch the homepage from the application

--- reservationSystem/bookings/admin.py
+++ reservationSystem/bookings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(RouteSchedule)
+admin.site.register(Flight)
+admin.site.register(Booking)
+admin.site.register(Passenger)
