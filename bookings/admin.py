56283f730510477ffac4bb10ea2253a84541b7d5
svvyas <svyas5@asu.edu>
2018-10-13 17:12:32 -0700

    US-8 Task-37: Created database tables and migrated the changes from Postgres to Django Models.

--- bookings/admin.py
+++ bookings/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(RouteSchedule)
-admin.site.register(Flight)
-admin.site.register(Booking)
-admin.site.register(Passenger)
+admin.site.register(FlightInfo)
+admin.site.register(FlightDetails)
+admin.site.register(CustomerInfo)
+admin.site.register(BookingInfo)
