1dbd763183a4ac7ede9eb36825c53b83933ee3c7
svvyas <svyas5@asu.edu>
2018-10-13 23:09:26 -0700

    US-8 Task-31 Tested the rendering of data from back-end on the front-end.

--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import FlightInfo
 
 
 def home(request):
     return render(request, 'index.html')
 def newpay(request):
     return render(request, 'newpay1.html')
+
+def searchResults(request):
+    flightInfo = FlightInfo.objects.all()
+    flights = list()
+    for flight in flightInfo:
+        flights.append(flight.source)
+    return HttpResponse(flights)
