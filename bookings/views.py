390013ad1ad28364f56bb36064205f1dae15df62
rmohan11 <rmohan11@asu.edu>
2018-10-14 15:45:15 -0700

    US#8 - Task#41 & Task#42 connected the source and destination fields in the homepage to the setup table in the database replacing the hard codded values

--- bookings/views.py
+++ bookings/views.py
@@ -2,15 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import FlightInfo
 
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def home(request):
-    return render(request, 'index.html')
+    flight = FlightInfo.objects.all()
+    args = {'source': flight}
+
+    if request.method == 'POST':
+        src = request.POST['source']
+        dest = request.POST['destination']
+        count = request.POST['passengers']
+        date = request.POST['date']
+
+        #required_flights = flight.fliter(source = src)
+        return HttpResponse(src+dest+count+date)
+
+    else:
+        return render(request, 'index.html', args)
+
+
+
 def newpay(request):
     return render(request, 'newpay1.html')
 
-def searchResults(request):
+
+'''def searchResults(request):
     flightInfo = FlightInfo.objects.all()
     flights = list()
     for flight in flightInfo:
         flights.append(flight.source)
-    return HttpResponse(flights)
+    return HttpResponse(flights)'''
