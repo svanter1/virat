61caebddf3c87d98b0af083d9dc9eded073bee30
rmohan11 <rmohan11@asu.edu>
2018-10-15 13:56:14 -0700

    US#8 Task#49 Made the date field mandatory and included logic in bookings.views to avoid listing the flights if the date is less than current date

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import FlightInfo, FlightDetails
+import datetime
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -16,26 +16,39 @@ def home(request):
         dest = request.POST['destination']
         count = request.POST['passengers']
         date = request.POST['date']
+        cls = request.POST['class']
 
         required_flights = FlightInfo.objects.filter(source=src, destination=dest)
         args['details'] = []
-        for flt in required_flights:
-            date_trn = FlightDetails.objects.filter(flight_id=flt.flight_id, departure_date=date)
-            trn = FlightDetails.objects.filter(flight_id=flt.flight_id, available_eseats__gte=count, departure_date=date)
 
-            if not date_trn:
-                args['details'] += [[flt.source, flt.destination, flt.departure, flt.arrival,
-                                     flt.duration_hrs, flt.price]]
+        if date >= datetime.datetime.today().strftime('%Y-%m-%d'):
+            for flt in required_flights:
+                date_trn = FlightDetails.objects.filter(flight_id=flt.flight_id, departure_date=date)
 
-            elif trn:
-                args['details'] += [[flt.source, flt.destination, flt.departure, flt.arrival,
-                                     flt.duration_hrs, flt.price]]
+                if not date_trn:
+                    args['details'] += [[flt.source, flt.destination, date, flt.departure, flt.arrival,
+                                         flt.duration_hrs, flt.price, flt.flight_id]]
+
+                else:
+                    if cls:
+                        trn = FlightDetails.objects.filter(flight_id=flt.flight_id, available_bseats__gte=count,
+                                                       departure_date=date)
+                    else:
+                        trn = FlightDetails.objects.filter(flight_id=flt.flight_id, available_eseats__gte=count,
+                                                       departure_date=date)
+
+                    if trn:
+                        args['details'] += [[flt.source, flt.destination, date, flt.departure, flt.arrival,
+                                         flt.duration_hrs, flt.price, flt.flight_id]]
 
         return render(request, 'searchResults.html', args)
 
     else:
         return render(request, 'index.html', args)
 
+
+def passenger(request):
+    return render(request, 'passenger-details.html')
 
 def newpay(request):
     return render(request, 'newpay1.html')
