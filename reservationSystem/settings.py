9a34b41b5a2fc7ff6da69286841791e35cca84fe
svvyas <34081909+svvyas@users.noreply.github.com>
2018-11-04 14:04:11 -0700

    DB name change
    Changed the Database name to sync with local postgres.

--- reservationSystem/settings.py
+++ reservationSystem/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'reservationSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'virat_test',
+        'NAME': 'virat',
 	'USER': 'postgres',
 	'PASSWORD': 'postgres',
 	'HOST': 'localhost',
